4bec3e534ae8d2ac2d968fa21ae1fa6024fbfa2e
dikkowright <53313100+dikkowright@users.noreply.github.com>
2020-04-05 07:01:03 +0100

    testing my commit

--- src/main/java/model/Entity.java
+++ src/main/java/model/Entity.java
@@ -3,5 +3,6 @@ package model;
 public class Entity {
     public static void main(String[] args) {
         System.out.println("i love christ");
+        System.out.println(" i like my wife");
     }
 }
