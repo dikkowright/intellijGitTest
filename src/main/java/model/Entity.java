eda25124b3f8db62cb683f8339e9927ecdb8654f
dikkowright <53313100+dikkowright@users.noreply.github.com>
2020-04-05 07:28:45 +0100

    my if statement

--- src/main/java/model/Entity.java
+++ src/main/java/model/Entity.java
@@ -2,7 +2,17 @@ package model;
 
 public class Entity {
     public static void main(String[] args) {
-        System.out.println("i love christ");
-        System.out.println(" i like my wife");
+//        System.out.println("i love christ");
+//        System.out.println(" i like my wife");
+
+        String name;
+         name = "duro";
+
+
+        if (name == "duro") {
+            System.out.println("It is " + name);
+        }else{
+            System.out.println(" wrong ");
+        }
     }
 }
